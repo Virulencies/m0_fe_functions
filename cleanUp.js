8b3b1fb611526837e36233f13688578124c771fc
Theo McCray <tlmccraycray@gmail.com>
2023-09-30 22:05:16 -0600

    Complete cleanup

--- cleanUp.js
+++ cleanUp.js
@@ -7,15 +7,19 @@
     // using `node cleanUp.js` or copy and paste into a replit
 
 
-// EX 1:
-function askForName() { console.log("Hello, what is your name?") }
+// EX 1: Added "return" in code to return the info, not just print it.
+function askForName() { 
+  return "Hello, what is your name?";
+}
 
-askForName()
+console.log("Hello, what is your name?")
+ askForName
 
 
-// EX 2:
+// EX 2: Added console.log before return in order to print results of the function
 function addThreeNums(first, second, third) {
-var sum = first + second + third
+var sum = first + second + third;
+console.log(sum);
 return sum;
       }
 
@@ -23,8 +27,8 @@ addThreeNums(1, 2, 3);
 addThreeNums(4, 2, 7);
 
 
-// EX 3:
-func makeFreshPesto(){
+// EX 3:func changed to function
+function makeFreshPesto(){
   console.log("Buy ingredients: basil, parmesan, romano, olive oil, pine nuts, garlic, salt, pepper");
   console.log("Pulse basil and pine nuts");
   console.log("Add garlic and cheeses");
@@ -34,11 +38,13 @@ func makeFreshPesto(){
 makeFreshPesto();
 
 
-//  EX 4:
-function average(num1, num2) 
-  {
+//  EX 4:added console.log to print result and function name to call the function. Also added numbers to test.
+function average(num1, num2) {
 var sum = num1 + num2;
     var avg = sum / 2;
-
+console.log(avg)
 return avg
   }
+
+  average(1, 3)
+  
